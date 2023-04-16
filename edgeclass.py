699714c6e9673491634e58f919eb6aa22614f9bd
Woondu <73076434+MFDOOM-ab@users.noreply.github.com>
2023-04-16 18:24:37 -0400

    Update edgeclass.py

--- edgeclass.py
+++ edgeclass.py
@@ -1,19 +1,18 @@
-# DFS Function with adjacent list to classify edges
-
 def dfs(u, adj, visited, parent, edges):
 
     visited[u] = True
     for v in adj[u]:
         if not visited[v]:
             edges.append((u, v, "tree"))
-            dfs(v, adj, visited, u, edges)
+            parent[v] = u
+            dfs(v, adj, visited, parent, edges)
         elif v != parent[u]:
-            if visited[v] == True and v in parent:
+            if visited[v] == True and parent[v] != -1:
                 edges.append((u, v, "back"))
             else:
                 edges.append((u, v, "forward"))
 
-def edgeClass(s, edges):
+def edge_classification(s, edges):
 
     n = len(s)
     adj = {}
@@ -23,12 +22,14 @@ def edgeClass(s, edges):
         adj[e[0]].add(e[1])
         adj[e[1]].add(e[0])
     visited = [False] * n
+    parent = [-1] * n  # initialize the parent dictionary
     for i in range(n):
         if not visited[i]:
-            dfs(i, adj, visited, {}, edges)
+            dfs(i, adj, visited, {i: -1}, edges)  # pass an initial value for the parent of the root node
     return edges
 
-# EXAMPLE -
+# Example usage:
 s = {0, 1, 2, 3, 4}
 edges = [(0, 1), (0, 2), (1, 3), (1, 4), (2, 3)]
-print(edgeClass(s,edges))
+classes = edge_classification(s, edges)
+print(classes)
